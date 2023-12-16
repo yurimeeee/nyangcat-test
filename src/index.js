67de414c2249a6263abf275ea354de4d04448ef5
yurimeeee <kplhn6@gmail.com>
2023-12-16 18:57:37 +0900

    build

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
+import React from "react";
 import App from "./App";
+// import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+console.log(process.env.PUBLIC_URL);
+root.render(
+  // <HashRouter>
+  <App />
+  // </HashRouter>
+);
